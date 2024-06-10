b7bcb1a6fad5d7355cc638a8fc90fc259200b460
anhoop89 <anhoop89@gmail.com>
2024-06-09 23:40:50 -0700

    set up abstract class

---
+++ Starter/Chef.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ this class is built for inheritance structure 
+ */
+namespace Starter
+{
+    abstract class Restaurant()
+    {
+        public abstract void Location();
+
+        public void Event()
+        {
+            Console.WriteLine("Hosting an event from the abstract class of ");
+        }
+    }
+    // super class 
+    internal class Chef: Restaurant
+    {
+        public Chef() {
+            Console.WriteLine("Constructor for the main parent class - Chef class");
+        }
+        public void MakeChicken()
+        {
+            Console.WriteLine("Chef Make Chicken! \n");
+        }
+
+        public void MakeFish()
+        {
+            Console.WriteLine("Chef Make Fish! \n");
+        }
+        // abstract function method
+        public override void Location()
+        {
+            Console.WriteLine("Location is overrided from the abstract class");
+        }
+        // virtual method
+        public virtual void MakeSpecialDish()
+        {
+            Console.WriteLine("The Chef makes bbq rids!\n");
+        }
+    }
+
+    // sub-classes
+    class VietnameseChef : Chef
+    {
+        public VietnameseChef()
+        {
+            Console.WriteLine("Contructor for the vietnamese chef class");
+        }
+
+        public void MakeVietDish()
+        {
+
+            Console.WriteLine("This Vietnamese Chef makes vietnamese dishes!\n");
+        }
+      
+        public override void MakeSpecialDish()
+        {
+            base.MakeSpecialDish();
+            Console.WriteLine("Vietnamese Chef can do Vietnamese Pho!\n");
+        }
+    }
+}
